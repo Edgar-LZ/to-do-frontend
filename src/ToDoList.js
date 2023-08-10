008ad9861c157b9f141b79b35f16ca5a0bc78247
Edgar Lopez <edgar.lopez@EDCMXENLT0763.local>
2023-08-09 22:01:10 -0600

    Get, uppdate, and filtering working. TODO: Add row, delete, and mark as done.

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,8 +1,9 @@
 
 import { useState, useEffect, useContext, createContext } from 'react';
-import { DataGrid } from '@mui/x-data-grid';
+import { GridRowModes, DataGrid, useGridApiContext, useGridApiRef } from '@mui/x-data-grid';
 import { Pagination } from '@mui/material';
-
+import {Button} from '@mui/material'
+import { useCallback } from 'react';
 
 import SearchForm from './Form';
 
@@ -21,12 +22,28 @@ export const ToDoContext = createContext(
    }
 );
 
+function padTo2Digits(num) {
+   return num.toString().padStart(2, '0');
+ }
+ 
+ function formatDate(date) {
+   return [
+     padTo2Digits(date.getDate()),
+     padTo2Digits(date.getMonth() + 1),
+     date.getFullYear(),
+   ].join('/');
+ }
+
 function ToDoList(){
+
+
+    const apiRef = useGridApiRef();
     const [rows, setRows ] = useState([]); 
     const [pages, setPages] = useState(1);
     const [currentPage, setCurrentPage] = useState(1);
     const [urlParams, setUrlParams] = useState("?");
     const [baseuri, setBaseUri] = useState('http://localhost:9090/todos');
+    const [rowModesModel, setRowModesModel] = useState({});
    
 
     useEffect(() => {
@@ -38,7 +55,6 @@ function ToDoList(){
                element.dueDate = new Date(/\d{4}-\d{2}-\d{2}/.exec(element.dueDate)[0].replace(/-/g, '\/'));
            }
          });
-         console.log(data);
          setRows(data.toDos);
          setPages(data.pages);
          setCurrentPage(data.currentPage);
@@ -51,28 +67,135 @@ function ToDoList(){
      function changePage(e, value) { 
       setUrlParams( new URLSearchParams({
             page: value,
-        }));
-      
+        })); 
       }
+
       function handleSortModelChange(model, details) {
          if(details == []) {
             
          }
       }
+       
+
+      function onEditButtonClick(e, row) {
+         
+         setRowModesModel({ ...rowModesModel, [row.id]: { mode: GridRowModes.Edit } });
+         console.log(rowModesModel);
+      }
+      
+      function onSaveButtonClick(e, row) {
+         setRowModesModel({ ...rowModesModel, [row.id]: { mode: GridRowModes.View } });
+
+         
+      }
+
+      function onCancelButtonClick(e, row) {
+         setRowModesModel({
+            ...rowModesModel,
+            [row.id]: { mode: GridRowModes.View, ignoreModifications: true },
+          });
+      }
+
+      function onDeleteButtonClick(e, row) {
+      }
+
+      function mySaveOnServerFunction(row, oldRow) {
+         console.log(row);
+         fetch(baseuri + "/" + row.id + "?"+ "text=" + row.text + "&dueDate=" +formatDate(row.dueDate) + "&priority=" + row.priority ,{ method: 'PUT', mode:'cors'})
+         .then((response) => response.json())
+         .catch((e) => {console.log(e)});
+         return row;
+      }
+      function handleUpdateError(err) {
+         console.log(err);
+      }
 
       const columns = [
-        { field: 'done', headerName: 'Done', type: "boolean", width: 150, editable: true },
-        { field: 'text', headerName: 'Name', width: 150, editable: true},
-        { field: 'priority', headerName: 'Priority', type: "singleSelect",valueOptions: ["High", "Medium", "Low"] ,width: 150, editable: true},
-        { field: 'dueDate', headerName: 'Due Date', type:"date", width: 150, editable: true},
+        { field: 'done', headerName: 'Done', type: "boolean", width: 150, editable: true, sortable:false, disableColumnMenu:true},
+        { field: 'text', headerName: 'Name', width: 150, editable: true, sortable:false, disableColumnMenu:true},
+        { field: 'priority', headerName: 'Priority', type: "singleSelect",valueOptions: ["High", "Medium", "Low"] ,width: 150, editable: true, disableColumnMenu:true},
+        { field: 'dueDate', headerName: 'Due Date', type:"date", width: 150, editable: true, disableColumnMenu:true},
+        {
+         field: "deleteButton",
+         headerName: "Actions",
+         description: "Actions column.",
+         sortable: false,
+         disableColumnMenu:true,
+         width: 160,
+         renderCell: (params) => {
+          const isInEditMode = rowModesModel[params.row.id]?.mode === GridRowModes.Edit;
+
+           if(isInEditMode) {
+               return (
+                  <span>
+                  <Button variant="text"
+                  onClick={
+                     (e) => {
+                        onSaveButtonClick(e, params.row)
+                     }
+                  }
+                  >
+                  Save
+                  </Button>
+                  <Button variant="text"
+                  onClick={
+                     (e) => {
+                        onCancelButtonClick(e, params.row)
+                     }
+                  }
+               >
+                  Cancel
+               </Button>
+               </span>
+               );
+             
+
+           }
+
+           return (
+            <span>
+             <Button variant="text"
+               onClick={
+                  (e) => {
+                     onEditButtonClick(e, params.row)
+                  }
+               }
+             >
+               Edit
+             </Button>
+             <Button variant="text"
+               onClick={
+                  (e) => {
+                     onDeleteButtonClick(e, params.row)
+                  }
+               }
+             >
+               Delete
+             </Button>
+             </span>
+           );
+         }
+       }
       ];
     
     return (
         <div>
             <ToDoContext.Provider value={{rows, setRows, pages, setPages, currentPage, setCurrentPage, urlParams, setUrlParams, baseuri, setBaseUri}}>
             <SearchForm/>
-            <DataGrid editMode="row" rows={rows} columns={columns} hideFooter onSortModelChange={handleSortModelChange}/>
-            <Pagination count={pages} page={currentPage} onChange={changePage} showLastButton showFirstButton />
+            <DataGrid editMode="row" 
+                        rows={rows}
+                        columns={columns} 
+                        hideFooter 
+                        rowModesModel={rowModesModel}
+                        onSortModelChange={handleSortModelChange} 
+                        apiRef={apiRef}
+                        processRowUpdate={(updatedRow, originalRow) =>
+                           mySaveOnServerFunction(updatedRow, originalRow)
+                         }
+                        onProcessRowUpdateError = {handleUpdateError}
+
+            />
+            <Pagination count={pages} page={currentPage} onChange={changePage} showLastButton showFirstButton/>
             </ToDoContext.Provider>
         </div>
      );
