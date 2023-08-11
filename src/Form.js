e57789c60a02f90013f5eab4d8eec99db4fc520a
Edgar-LZ <edgarosv.lz@gmail.com>
2023-08-11 17:29:25 -0600

    Search form

--- src/Form.js
+++ src/Form.js
@@ -2,32 +2,40 @@
 import { useState } from 'react';
 import { useContext } from 'react';
 import { ToDoContext } from './ToDoList';
+import Select from '@mui/joy/Select';
+import Option from '@mui/joy/Option';
+import { Textarea } from '@mui/joy';
+import {Button} from '@mui/material'
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+
 
 function SearchForm() {
 
     
-    const {rows, setRows, pages, setPages, currentPage, setCurrentPage, urlParams, setUrlParams, baseuri, setBaseUri}  = useContext(ToDoContext);
+    const {rows, setRows, pages, setPages, currentPage, setCurrentPage, urlParams, setUrlParams, baseuri, setBaseUri, averageTime}  = useContext(ToDoContext);
 
     const [search, setSearch] = useState('');
     const [priority, setPriority] = useState('All');
     const [status, setStatus] = useState('All');
 
     function handleClick(){
-        setUrlParams(new URLSearchParams(
+        setUrlParams(
             {
                 filterName: search,
                 filterPriority: priority,
                 filterDone:status,
             }
-        ));
+        );
+        setSearch('');
 
     }
 
-    function handlePriorityChange(e) {
-        setPriority(e.target.value);
+    function handlePriorityChange(e, value) {
+        setPriority(value);
     }
-    function handleStatusChange(e) {
-        setStatus(e.target.value);
+    function handleStatusChange(e, value) {
+        setStatus(value);
     }
     function handleTextareaChange(e) {
         setSearch(e.target.value);
@@ -35,35 +43,47 @@ function SearchForm() {
     
     
     return (
-        <div>
-                <span>
-                    <label>
-                        Name:{''} <textarea value={search} onChange={handleTextareaChange} />
-                    </label>
-                </span>
+        <Box sx={{ flexGrow: 1 , p: 5}}>
+        <Box sx={{ flexGrow: 1, border: '2px solid lightgray', }}>
 
-                <span>
-                    <label> Priority:{''}
-                    <select className="dropdown" onChange={handlePriorityChange}>
-                    <option value="All">All</option>
-                    <option value="High">High</option>
-                    <option value="Medium">Medium</option>
-                    <option value="Low">Low</option>
-                    </select>
-                    </label>
-                </span>
+        <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(3, 3fr)' , p:2}}>
+        <label htmlFor="text-area" id="select-label">Name: </label>
+                        <Textarea
+                            value={search}
+                            onChange={handleTextareaChange}
+                            placeholder="Search term..."
+                         sx={{ mb: 1 }}
+                        />
+        </Box>
                 
-                <span>
-                    <label> State:{''}
-                    <select className="dropdown" onChange={handleStatusChange}>
-                        <option value="All">All</option>
-                        <option value="Done">Done</option>
-                        <option value="Undone">Undone</option>
-                    </select>
-                    </label>
-                    <button name="send" id="send" onClick={handleClick}> Submit  </button>
-                </span>
-        </div>
+                    
+
+         <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(6, 1fr)', p:2 }} >
+                
+                <label htmlFor="select-button" id="select-label">Priority: </label>
+                <Select defaultValue="All" onChange={handlePriorityChange}>
+                    <Option value="All">All</Option>
+                    <Option value="High">High</Option>
+                    <Option value="Medium">Medium</Option>
+                    <Option value="Low">Low</Option>
+                </Select>
+        </Box>
+                
+        <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)' , p:2}} >
+             <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)' }} >
+                <label htmlFor="select-button" id="select-label">State: </label>
+                <Select defaultValue="All" onChange={handleStatusChange}>
+                    <Option value="All">All</Option>
+                    <Option value="Done">Done</Option>
+                    <Option value="Undone">Undone</Option>
+                </Select>
+                <Box/>
+                </Box>
+             <Button onClick={handleClick}> Submit</Button>
+         </Box>
+
+    </Box>
+    </Box>
      );
 
 }
